fcbc45b5a95ba6d9171c71e092d658a6f7984451
arnobarzan <arno.barzan@pxl.be>
2024-11-24 21:43:28 +0100

    Initial commit

---
+++ BethanysPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(items, _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
---
+++ BethanysPieShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCartViewModel(IEnumerable<ShoppingCartItem> shoppingCartItems, decimal shoppingCartTotal)
+        {
+            ShoppingCartItems = shoppingCartItems;
+            ShoppingCartTotal = shoppingCartTotal;
+        }
+
+        public IEnumerable<ShoppingCartItem> ShoppingCartItems { get; }
+        public decimal ShoppingCartTotal { get; }
+    }
+}
